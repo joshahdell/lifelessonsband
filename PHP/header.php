bc2c2b8fd57a0e645d6b77e03f7a1777803b3bcb
joshahdell <josh_odell@suddenlink.net>
2016-09-21 10:16:40 -0500

    php
    php

---
+++ PHP/header.php
@@ -0,0 +1,53 @@
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/lifelessonsband/css?family=Ubuntu" rel="stylesheet">
+    <link href="https://joshahdell.github.io/lifelessonsband/CSS/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" type="text/css" href="https://joshahdell.github.io/lifelessonsband/CSS/main.css">
+    <style>
+
+    </style>
+</head>
+
+<body>
+
+<div style="background-color: rgba(210, 210, 161, .8);">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 socialicons">
+            <ul>
+                <li><a href="https://www.facebook.com/lifelessonsok" target="_blank"><img src="images/1474192301_facebook.png" width="30"></a></li>
+                <li><a href="https://twitter.com/lifelessonsok" target="_blank"><img src="images/1474192302_twitter.png" width="30"></a></li>
+                <li><a href="https://www.instagram.com/lifelessonsok/" target="_blank"><img src="images/1474192311_instagram.png" width="30"></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="row headerlogo">
+    <div class="col-lg-12">
+        <img src="https://joshahdell.github.io/lifelessonsband/images/lllogodark.png" width="600" class="lllogo">
+     </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <br>
+
+            <nav>
+                <ul>
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="tour.html">TOUR</a></li>
+                    <li>VIDEOS</li>
+                    <li><a href="http://lifelessonsband.bandcamp.com/merch">STORE</a></li>
+                    <li>CONTACT</li>
+                </ul>
+            </nav>
+            <div class="rectangle"></div>
+            <br>
+        </div>
+    </div>
+</div>
